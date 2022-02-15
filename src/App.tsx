d58cb58986507f0ac05f590894ef194164c4656e
Hakan Ergoksen <hakan@instarc.com>
2022-02-15 13:22:47 +0300

    Added proccess.env.name

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import testSvgImage from './test-checkmark.svg'
 
 export const App = () => {
     return <>
-    <h1>React TypeScript</h1>
+    <h1>React TypeScript - Mode: {process.env.NODE_ENV} {process.env.name}</h1>
     <img src={testImage} alt="Test Image" width="250" />
     <img src={testSvgImage} alt="Test Checkmark SVG" width="250" />
     </>
